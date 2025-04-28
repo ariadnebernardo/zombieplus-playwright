259856bae873edd36f7d656a3cd19d9bb0919d49
Ariadne Bernardo <ariadne.bernardoqa@gmail.com>
2025-04-28 20:11:43 -0300

    Aumentando cobertura de testes e concertando bug no arquivo ..//support/index.js

--- tests/e2e/movies.spec.js
+++ tests/e2e/movies.spec.js
@@ -6,7 +6,6 @@ const { executeSQL } = require('../support/database')
 
 test('Deve poder cadastrar um novo filme', async ({ page }) => {
     const movie = data.guerra_mundial_z
-
     await executeSQL(`DELETE FROM public.movies WHERE title = '${movie.title}';`)
 
     await page.login.visit()
@@ -17,4 +16,20 @@ test('Deve poder cadastrar um novo filme', async ({ page }) => {
 
     const message = 'Cadastro realizado com sucesso!'
     await page.toast.containText(message)
+})
+
+test('Não deve cadastrar quando os campos brigatórios não são preenchidos', async ({ page }) => {
+    await page.login.visit()
+    await page.login.submit('admin@zombieplus.com', 'pwd123')
+    await page.movies.isLoggedIn()
+
+    await page.movies.goForm()
+    await page.movies.submit()
+
+    await page.movies.alertHaveText([
+        'Por favor, informe o título.',
+        'Por favor, informe a sinopse.', 
+        'Por favor, informe a empresa distribuidora.', 
+        'Por favor, informe o ano de lançamento.'
+    ])
 })
--- tests/pages/MoviesPage.js
+++ tests/pages/MoviesPage.js
@@ -11,8 +11,16 @@ export class MoviesPage {
         await expect(this.page).toHaveURL(/.*admin/)
     }
 
-    async create(title, overview, company, release_year) {
+    async goForm() {
         await this.page.locator('a[href$="register"]').click()
+    }
+
+    async submit() {
+        await this.page.getByRole('button', { name: 'Cadastrar' }).click()
+    }
+
+    async create(title, overview, company, release_year) {
+        await this.goForm()
         await this.page.getByLabel('Titulo do filme').fill(title)
         await this.page.getByLabel('Sinopse').fill(overview)
 
@@ -30,6 +38,10 @@ export class MoviesPage {
             .filter({ hasText: release_year })
             .click()
 
-        await this.page.getByRole('button', { name: 'Cadastrar' }).click()
+        await this.submit()
+    }
+
+    async alertHaveText(target) {
+        await expect(this.page.locator('.alert')).toHaveText(target)
     }
 }
